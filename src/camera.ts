7fc29f435793c9944691224bdffbdf99dc397204
Jesper Lund <jeslu246@student.liu.se>
2018-11-07 13:38:06 +0100

    add sky sphere

--- src/camera.ts
+++ src/camera.ts
@@ -5,15 +5,15 @@ export class Camera extends THREE.PerspectiveCamera {
     constructor(aspectRatio) {
         super(60, aspectRatio,0.01, 100);
 
-        this.position.set( 5, 4, 5 );
+        this.position.set( 50, 40, 50 );
         this.lookAt( 0, 1.5, 0 );
     }
 
     update() {
         var time = performance.now() / 5000;
 
-        this.position.x = Math.sin( time ) * 5;
-        this.position.z = Math.cos( time ) * 5;
+        this.position.x = Math.sin( time ) * 3;
+        this.position.z = Math.cos( time ) * 3;
         this.lookAt( 0, 1.5, 0 );
     }
 }
--- src/scene.ts
+++ src/scene.ts
@@ -4,11 +4,15 @@ import * as THREE from 'three'
 export class Scene extends THREE.Scene {
     constructor() {
         super();
-        // add axis to the scene
-        // let axis = new THREE.AxesHelper(2)
-        // this.add(axis);
 
-        this.add( new THREE.GridHelper( 10, 10 ) );
+        const sceneRadius = 50;
+        // add axis to the scene
+        let axis = new THREE.AxesHelper(2)
+        this.add(axis);
+
+        this.background = new THREE.Color( 0x222222 );
+
+        this.add( new THREE.GridHelper( 100, 100 ) );
 
         // add lights
         let light = new THREE.DirectionalLight(0xffffff, 1.0)
@@ -19,6 +23,12 @@ export class Scene extends THREE.Scene {
         ambient.position.set( -0.5, 0.75, -1 );
         this.add( ambient );
 
-        this.background = new THREE.Color( 0x222222 );
+        var geometry = new THREE.SphereGeometry(sceneRadius, 32, 32, 0, Math.PI );
+        var skyMaterial = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
+        var sky = new THREE.Mesh( geometry, skyMaterial );
+        skyMaterial.side = THREE.BackSide;
+        sky.rotateX(-Math.PI/2);
+        this.add( sky );
+
     }
 }
