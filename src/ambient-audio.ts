59ddaef8e78c7ab9e48251987bfb9f5213322818
Jesper Lund <jeslu246@student.liu.se>
2018-11-09 20:41:41 +0100

    add FOA audio

---
+++ src/ambient-audio.ts
@@ -0,0 +1,25 @@
+import * as THREE from 'three'
+
+export class FOAmbisonics { 
+    constructor(audioName) {  
+
+        // Set up an audio element to feed the ambisonic source audio feed.
+        // Input audio should contain 4 channels (foa)
+        var audioElement = document.createElement('audio');
+        audioElement.src = '../assets/audio/' + audioName;
+
+        // Create AudioContext, MediaElementSourceNode and FOARenderer.
+        var audioContext = new AudioContext();
+        var audioElementSource =
+            audioContext.createMediaElementSource(audioElement);
+        var foaRenderer = Omnitone.createFOARenderer(audioContext);
+
+        // Make connection and start play.
+        foaRenderer.initialize().then(function() {
+            audioElementSource.connect(foaRenderer.input);
+            foaRenderer.output.connect(audioContext.destination);
+            audioElement.volume = 0.3;
+            audioElement.play();
+        });
+    }
+}
