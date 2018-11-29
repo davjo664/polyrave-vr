4594cf7e9179750aff9e9667775055a1b09e3276
Jesper Lund <jeslu246@student.liu.se>
2018-11-29 10:01:11 +0100

    add intro container

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import './style.css'
 import {Main} from './main'
+import { CullFaceNone } from 'three';
 
 function init() {
 	const container = document.getElementById('appContainer');
+
+	const introSceneContainer = document.getElementById('introSceneContainer');
+
+	setTimeout(() => {
+		container.style.display = 'none';
+		container.style.visibility = 'hidden';
+		container.style.opacity = '0';
+		new Main(introSceneContainer);
+	}, 3000);
+
 	new Main(container);
 }
 
